346d75a898cd898ca23bbfac01bb91832e99124d
Luc Ravenelle <lrav35@gmail.com>
2024-04-05 09:19:06 -0400

    adding new photos and generated ones from training run

--- fine_tune_dreambooth.py
+++ fine_tune_dreambooth.py
@@ -176,32 +176,6 @@ def parse_args():
     args = parser.parse_args()
     
     return args
-
-
-
-# !accelerate launch train_dreambooth_inpaint.py \
-#     --pretrained_model_name_or_path="runwayml/stable-diffusion-inpainting"  \
-#     --instance_data_dir="images/dog" \
-#     --output_dir="stable-diffusion-inpainting-dog" \
-#     --instance_prompt="a photo of a sks dog" \
-#     --resolution=512 \
-#     --mixed_precision="fp16" \
-#     --train_batch_size=1 \
-#     --learning_rate=5e-6 \
-#     --lr_scheduler="constant" \
-#     --lr_warmup_steps=0 \
-#     --max_train_steps=500 \
-#     --gradient_accumulation_steps=2 \
-#     --gradient_checkpointing \
-#     --train_text_encoder \
-#     --seed="0"
-    
-
-# accelerate launch fine_tune_dreambooth.py \
-#     --prompt="insert prompt here" \
-#     --input_data_dir="images/" \
-#     --max_train_steps=500 \
-#     --train_batch_size=1 \
     
 def main():
     """
